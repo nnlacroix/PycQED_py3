756671831b16eec5151c6900281c54f5d84d99a2
Christoph Hellings <chellings@phys.ethz.ch>
2020-04-29 18:02:38 +0200

    new module circuit_builder: - create blocks for routine tasks (initialization, readout) - create a block from a list of operations

---
+++ pycqed/measurement/waveform_control/circuit_builder.py
@@ -0,0 +1,312 @@
+from copy import deepcopy
+from pycqed.measurement.waveform_control.block import Block
+
+class CircuitBuilder:
+
+    STD_INIT = {'0': 'I', '1': 'X180', '+': 'Y90', '-': 'mY90'}
+
+    def __init__(self, qb_names, operation_dict, **kwargs):
+        self.qb_names = qb_names
+        self.operation_dict = operation_dict
+        self.cz_pulse_name = kwargs.get('cz_pulse_name', 'upCZ')
+
+    def get_qubits(self, qubits='all'):
+        """
+        Wrapper to get 'all' qubits, single qubit specified as string
+        or list of qubits, checking they are in self.qb_names
+        :param qubits: 'all', single qubit name (eg. 'qb1') or list of qb names
+        :return: list of qb names
+        """
+        if qubits == 'all':
+            return self.qb_names
+        elif qubits in self.qb_names:  # qubits == single qb name eg. 'qb1'
+             qubits = [qubits]
+        for qb in qubits:
+            assert qb in self.qb_names, f"{qb} not found in {self.qb_names}"
+        return qubits
+
+    def get_pulse(self, op, parse_z_gate=False):
+        """
+        Gets a pulse from the operation dictionary, and possibly parses
+        arbitrary angle from Z gate operation.
+        Examples:
+             >>> get_pulse(['Z100 qb1'], parse_z_gate=True)
+             will perform a 100 degree Z rotation
+        Args:
+            op: operation
+            parse_z_gate: whether or not to look for Zgates with arbitrary angles.
+
+        Returns: deepcopy of the pulse dictionary
+
+        """
+        if parse_z_gate and op.startswith("Z"):
+            # assumes operation format of f"Z{angle} qbname"
+            # FIXME: This parsing is format dependent and is far from ideal but
+            #  until we can get parametrized pulses it is helpful to be able to
+            #  parse Z gates
+            angle, qbn = op.split(" ")[0][1:], op.split(" ")[1]
+            p = self.get_pulse(f"Z180 {qbn}", parse_z_gate=False)
+            p['basis_rotation'] = {qbn: float(angle)}
+        elif op.startswith("CZ"):
+            tmp = op.split(" ")
+            qba, qbb = tmp[1], tmp[2]
+            if f"{self.cz_pulse_name} {qba} {qbb}" in self.operation_dict:
+                p = deepcopy(
+                    self.operation_dict[f"{self.cz_pulse_name} {qba} {qbb}"])
+            elif f"{self.cz_pulse_name} {qbb} {qba}" in self.operation_dict:
+                p = deepcopy(
+                    self.operation_dict[f"{self.cz_pulse_name} {qbb} {qba}"])
+            else:
+                raise KeyError(f'CZ gate "{self.cz_pulse_name} {qba} {qbb}" not found.')
+        else:
+            p = deepcopy(self.operation_dict[op])
+        return p
+
+    def initialize(self, init_state='0', qubits='all', prep_params=None,
+                   simultaneous=True, block_name=None):
+        """
+        Initializes the specified qubits with the corresponding init_state
+        :param init_state (String or list): Can be one of the following
+            - one of the standard initializations: '0', '1', '+', '-'.
+              In that case the same init_state is done on all qubits
+            - list of standard init. Must then be of same length as 'qubits' and
+              in the same order.
+            - list of arbitrary pulses (which are in the operation_dict). Must be
+              of the same lengths as 'qubits' and in the same order. Should not
+              include space and qubit name (those are added internally).
+        :param qubits (list or 'all'): list of qubits on which init should be
+            applied. Defaults to all qubits.
+        :param prep_params: preparation parameters
+        :return: init segment
+        """
+        if block_name is None:
+            block_name = f"Initialization_{qubits}"
+        qubits = self.get_qubits(qubits)
+        if prep_params is None:
+            prep_params = {}
+        if len(init_state) == 1:
+            init_state = [init_state] * len(qubits)
+        else:
+            assert len(init_state) == len(qubits), \
+                "There must be a one to one mapping between initializations and " \
+                f"qubits. Got {len(init_state)} init and {len(qubits)} qubits"
+
+        pulses = []
+        pulses.extend(self.prepare(qubits, ref_pulse="start",
+                                   **prep_params).build())
+        for i, (qbn, init) in enumerate(zip(qubits, init_state)):
+            # add qb name and "s" for reference to start of previous pulse
+            op = self.STD_INIT.get(init, init) + \
+                 f"{'s' if len(pulses) != 0 and simultaneous else ''} " + qbn
+            pulse = self.get_pulse(op)
+            # if i == 0:
+            #     pulse['ref_pulse'] = 'segment_start'
+            pulses.append(pulse)
+        return Block(block_name, pulses)
+
+    def prepare(self, qubits='all', ref_pulse='start', preparation_type='wait',
+                post_ro_wait=1e-6, ro_separation=1.5e-6, reset_reps=3,
+                final_reset_pulse=False, threshold_mapping=None, block_name=None):
+        """
+        Prepares specified qb for an experiment by creating preparation pulse for
+        preselection or active reset.
+        Args:
+            qubits: which qubits to prepare. Defaults to all.
+            ref_pulse: reference pulse of the first pulse in the pulse list.
+                reset pulse will be added in front of this. If the pulse list is empty,
+                reset pulses will simply be before the block_start.
+            preparation_type:
+                for nothing: 'wait'
+                for preselection: 'preselection'
+                for active reset on |e>: 'active_reset_e'
+                for active reset on |e> and |f>: 'active_reset_ef'
+            post_ro_wait: wait time after a readout pulse before applying reset
+            ro_separation: spacing between two consecutive readouts
+            reset_reps: number of reset repetitions
+            final_reset_pulse: Note: NOT used in this function.
+            threshold_mapping (dict): thresholds mapping for each qb
+
+        Returns:
+
+        """
+        if block_name is None:
+            block_name = f"Preparation_{qubits}"
+        qb_names = self.get_qubits(qubits)
+
+        if threshold_mapping is None or len(threshold_mapping) == 0:
+            threshold_mapping = {qbn: {0: 'g', 1: 'e'} for qbn in qb_names}
+
+        # Calculate the length of a ge pulse, assumed the same for all qubits
+        state_ops = dict(g=["I "], e=["X180 "], f=["X180_ef ", "X180 "])
+
+        # no preparation pulses
+        if preparation_type == 'wait':
+            return Block(block_name, [])
+
+        # active reset
+        elif 'active_reset' in preparation_type:
+            reset_ro_pulses = []
+            ops_and_codewords = {}
+            for i, qbn in enumerate(qb_names):
+                reset_ro_pulses.append(self.get_pulse('RO ' + qbn))
+                reset_ro_pulses[-1]['ref_point'] = 'start' if i != 0 else 'end'
+
+                if preparation_type == 'active_reset_e':
+                    ops_and_codewords[qbn] = [
+                        (state_ops[threshold_mapping[qbn][0]], 0),
+                        (state_ops[threshold_mapping[qbn][1]], 1)]
+                elif preparation_type == 'active_reset_ef':
+                    assert len(threshold_mapping[qbn]) == 4, \
+                        "Active reset for the f-level requires a mapping of length 4" \
+                            f" but only {len(threshold_mapping)} were given: " \
+                            f"{threshold_mapping}"
+                    ops_and_codewords[qbn] = [
+                        (state_ops[threshold_mapping[qbn][0]], 0),
+                        (state_ops[threshold_mapping[qbn][1]], 1),
+                        (state_ops[threshold_mapping[qbn][2]], 2),
+                        (state_ops[threshold_mapping[qbn][3]], 3)]
+                else:
+                    raise ValueError(f'Invalid preparation type {preparation_type}')
+
+            reset_pulses = []
+            for i, qbn in enumerate(qb_names):
+                for ops, codeword in ops_and_codewords[qbn]:
+                    for j, op in enumerate(ops):
+                        reset_pulses.append(self.get_pulse(op + qbn))
+                        reset_pulses[-1]['codeword'] = codeword
+                        if j == 0:
+                            reset_pulses[-1]['ref_point'] = 'start'
+                            reset_pulses[-1]['pulse_delay'] = post_ro_wait
+                        else:
+                            reset_pulses[-1]['ref_point'] = 'start'
+                            pulse_length = 0
+                            for jj in range(1, j + 1):
+                                if 'pulse_length' in reset_pulses[-1 - jj]:
+                                    pulse_length += reset_pulses[-1 - jj]['pulse_length']
+                                else:
+                                    pulse_length += reset_pulses[-1 - jj]['sigma'] * \
+                                                    reset_pulses[-1 - jj]['nr_sigma']
+                            reset_pulses[-1]['pulse_delay'] = post_ro_wait + pulse_length
+
+            prep_pulse_list = []
+            for rep in range(reset_reps):
+                ro_list = deepcopy(reset_ro_pulses)
+                ro_list[0]['name'] = 'refpulse_reset_element_{}'.format(rep)
+
+                for pulse in ro_list:
+                    pulse['element_name'] = 'reset_ro_element_{}'.format(rep)
+                if rep == 0:
+                    ro_list[0]['ref_pulse'] = ref_pulse
+                    ro_list[0]['pulse_delay'] = -reset_reps * ro_separation
+                else:
+                    ro_list[0]['ref_pulse'] = 'refpulse_reset_element_{}'.format(
+                        rep - 1)
+                    ro_list[0]['pulse_delay'] = ro_separation
+                    ro_list[0]['ref_point'] = 'start'
+
+                rp_list = deepcopy(reset_pulses)
+                for j, pulse in enumerate(rp_list):
+                    pulse['element_name'] = 'reset_pulse_element_{}'.format(rep)
+                    pulse['ref_pulse'] = 'refpulse_reset_element_{}'.format(rep)
+                prep_pulse_list += ro_list
+                prep_pulse_list += rp_list
+
+            # manually add block_end with delay referenced to last readout
+            # as if it was an additional readout pulse
+            # otherwise next pulse will overlap with codeword padding.
+            block_end = dict(name='end', pulse_type="VirtualPulse",
+                             ref_pulse=f'refpulse_reset_element_{reset_reps-1}',
+                             pulse_delay=ro_separation)
+            prep_pulse_list += [block_end]
+            return Block(block_name, prep_pulse_list)
+
+        # preselection
+        elif preparation_type == 'preselection':
+            preparation_pulses = []
+            for i, qbn in enumerate(qb_names):
+                preparation_pulses.append(self.get_pulse('RO ' + qbn))
+                preparation_pulses[-1]['ref_point'] = 'start'
+                preparation_pulses[-1]['element_name'] = 'preselection_element'
+            preparation_pulses[0]['ref_pulse'] = ref_pulse
+            preparation_pulses[0]['name'] = 'preselection_RO'
+            preparation_pulses[0]['pulse_delay'] = -ro_separation
+            block_end = dict(name='end', pulse_type="VirtualPulse",
+                             ref_pulse='preselection_RO',
+                             pulse_delay=ro_separation)
+            preparation_pulses += [block_end]
+            return Block(block_name, preparation_pulses)
+
+    def mux_readout(self, qubits='all', element_name='RO',ref_point='end',
+                    pulse_delay=0.0):
+        block_name = "Readout"
+        qubits = self.get_qubits(qubits)
+        ro_pulses = []
+        for j, qb_name in enumerate(qubits):
+            ro_pulse = deepcopy(self.operation_dict['RO ' + qb_name])
+            ro_pulse['name'] = '{}_{}'.format(element_name, j)
+            ro_pulse['element_name'] = element_name
+            if j == 0:
+                ro_pulse['pulse_delay'] = pulse_delay
+                ro_pulse['ref_point'] = ref_point
+            else:
+                ro_pulse['ref_point'] = 'start'
+            ro_pulses.append(ro_pulse)
+        return Block(block_name, ro_pulses)
+
+    def Z_gate(self, theta=0, qubits='all'):
+
+        """
+        Software Z-gate of arbitrary rotation.
+
+        :param theta:           rotation angle, in degrees
+        :param qubits:      pulse parameters (dict)
+
+        :return: Pulse dict of the Z-gate
+        """
+
+        # if qubits is the name of a qb, expects single pulse output
+        single_qb_given = False
+        if qubits in self.qb_names:
+            single_qb_given = True
+        qubits = self.get_qubits(qubits)
+
+        pulses = [self.get_pulse(f'Z{theta} {qbn}', True) for qbn in qubits]
+        return pulses[0] if single_qb_given else pulses
+
+    def block_from_ops(self, block_name, operations, fill_values=None,
+                       pulse_modifs=None):
+        """
+        Returns a block with the given operations.
+        Eg.
+        >>> ops = ['X180 {qbt:}', 'X90 {qbc:}']
+        >>> builder.block_from_ops("MyAwesomeBlock",
+        >>>                                ops,
+        >>>                                {'qbt': qb1, 'qbc': qb2})
+        :param block_name: Name of the block
+        :param operations: list of operations (str), which can be preformatted
+            and later filled with values in the dictionary fill_values
+        :param fill_values (dict): optional fill values for operations.
+        :param pulse_modifs (dict): Modification of pulses parameters.
+            keys:
+             -indices of the pulses on  which the pulse modifications should be
+             made (backwards compatible)
+             -
+             values: dictionaries of modifications
+            E.g. ops = ["X180 qb1", "Y90 qb2"],
+            pulse_modifs = {1: {"ref_point": "start"}}
+            This will modify the pulse "Y90 qb2" and reference it to the start
+            of the first one.
+        :return:
+        """
+        if fill_values is None:
+            fill_values = {}
+        if pulse_modifs is None:
+            pulse_modifs = {}
+
+        pulses = [self.get_pulse(op.format(**fill_values), True)
+                  for op in operations]
+
+        # modify pulses
+        [pulses[i].update(pm) for i, pm in pulse_modifs.items()]
+        return Block(block_name, pulses)
+
