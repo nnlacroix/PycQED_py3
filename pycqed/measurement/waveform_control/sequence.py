3e1dc50eacbfcc97ca5f1ed28432833b0f343e1d
Nathan Lacroix <lna@student.ethz.ch>
2020-05-27 10:30:56 +0200

    Add merging feature for sequences
    - Add merge function to Sequence, allows to merge several sequences into
      one or more, depending on the segment_limit
    - Add deepcopy function to Segment and Sequence, as they cannot be
      blindly deepcopied because of the reference to the Pulsar instrument.

--- pycqed/measurement/waveform_control/sequence.py
+++ pycqed/measurement/waveform_control/sequence.py
@@ -7,7 +7,9 @@
 import numpy as np
 import pycqed.measurement.waveform_control.pulsar as ps
 from collections import OrderedDict as odict
-
+from copy import deepcopy
+import logging
+log = logging.getLogger(__name__)
 
 class Sequence:
     """
@@ -24,7 +26,7 @@ class Sequence:
 
     def add(self, segment):
         if segment.name in self.segments:
-            raise Exception('Name {} already exisits in the sequence!'.format(
+            raise NameError('Name {} already exisits in the sequence!'.format(
                 segment.name))
         self.segments[segment.name] = segment
 
@@ -99,6 +101,12 @@ class Sequence:
             n_readouts = np.sum(n_readouts)
         return n_readouts
 
+    def n_segments(self):
+        """
+        Gets the number of segments in the sequence.
+        """
+        return len(self.segments)
+
     def repeat(self, pulse_name, operation_dict, pattern,
                pulse_channel_names=('I_channel', 'Q_channel')):
         """
@@ -132,11 +140,129 @@ class Sequence:
         """
         return self.repeat(pulse_name, operation_dict,
                            (self.n_acq_elements(), 1))
+
+
+    @staticmethod
+    def merge(sequences, segment_limit=None, merge_repeat_patterns=True):
+        """
+        Merges a list of sequences. See documentation of Sequence.__add__()
+        for more information on the merge of two sequences.
+        Args:
+            sequences (list): List of sequences to merge
+            segment_limit (int): maximal number of segments in the merged sequence.
+                if the total number of segments is higher, a list of sequences is
+                returned. Default is None (all sequences are merged)
+            merge_repeat_patterns (bool): Merges the readout pattern when
+                 combining the sequences. If the readout pattern already exists, it adds
+                 to the number of repetition of the pattern. Note that this behavior may
+                 not work for all scenarios. In that case the patterns must be updated
+                  manually after the merge and merge_repeat_patterns should be set to
+                  False. Default: True.
+
+
+        Returns: list of merged sequences
+
+        Examples:
+            >>> # No segment_limit
+            >>> seq1 = Sequence('seq1')
+            >>> seq1.extend(segments_of_seq1)  # 10 segments
+            >>> seq2 = Sequence('seq2')
+            >>> seq2.extend(segments_of_seq2) # 15 segments
+            >>> seq_comb = Sequence.merge([seq1, seq2])
+            >>> # returns a list with 1 sequence with 25 segments
+            >>> # i.e. [seq1 + seq2]
+
+            >>> # 20 segments limit
+            >>> seq1 = Sequence('seq1')
+            >>> seq1.extend(segments_of_seq1) # 10 segments
+            >>> seq2 = Sequence('seq2')
+            >>> seq2.extend(segments_of_seq2) # 15 segments
+            >>> seq3 = Sequence('seq3')
+            >>> seq3.extend(segments_of_seq3) # 5 segments
+            >>> seq_comb = Sequence.merge([seq1, seq2, seq3])
+            >>> # returns list of 2 sequences with 10 and 20 segments,
+            >>> # i.e. [seq1, seq2 + seq3]
+
+
+        """
+        if len(sequences) == 0:
+            raise ValueError("merge requires at least one sequence")
+        elif len(sequences) == 1:
+            # special case, return current sequence:
+            return sequences
+        sequences = [deepcopy(s) for s in sequences]
+        merged_seqs = [sequences[0]]
+        if segment_limit is None:
+            segment_limit = np.inf
+
+        segment_counter = sequences[0].n_segments()
+        seg_occurences = [{s: 1 for s in sequences[0].segments}]
+        for seq in sequences[1:]:
+            assert seq.n_segments() <= segment_limit, \
+                f"Sequence {seq.name} has more segments ({seq.n_segments()})" \
+                f" than the segment_limit ({segment_limit}). Cannot merge " \
+                f"without cropping the sequence."
+            # if over segment_limit, add another separate sequence
+            # to merged sequences
+            if merged_seqs[-1].n_segments() + seq.n_segments() > segment_limit:
+                merged_seqs.append(seq)
+                seg_occurences.append({s: 1 for s in seq.segments})
+                segment_counter = seq.n_segments()
+            # otherwise merge sequences
+            else:
+                for seg_name, segment in seq.segments.items():
+                    try:
+                        merged_seqs[-1].add(segment)
+                    except NameError:  # in case segment name exists, create new name
+                        seg_occurences[-1][seg_name] += 1
+                        new_name =seg_name + \
+                                   f"_copy_from_merge_{seg_occurences[-1][seg_name] - 1}"
+                        segment.name = new_name
+                        merged_seqs[-1].add(segment)
+
+                segment_counter += seq.n_segments()
+
+                merged_seqs[-1].name += "+" + seq.name # update name of merged seq
+                if merge_repeat_patterns:
+                    for ch_name, pattern in seq.repeat_patterns.items():
+                        # if channel is already present, update number of
+                        # repetitions
+                        if ch_name in merged_seqs[-1].repeat_patterns:
+                            pattern_prev = \
+                                merged_seqs[-1].repeat_patterns[ch_name]
+                            if pattern_prev[1] != pattern[1]:
+                                raise NotImplementedError(
+                                    "The repeat patterns for channel: {ch_name} do not "
+                                    f"have the same 'outer loop' specification (see "
+                                    f"docstring Sequence.repeat). Repeat patterns cannot "
+                                    f"be merged automatically. Set merge_repeat_patterns "
+                                    f"to False and update the repeat patterns manually.")
+                            pattern_updated = (pattern_prev[0] + pattern[0],
+                                               pattern_prev[1])
+                            merged_seqs[-1].repeat_patterns[ch_name] = pattern_updated
+                        # add repeat pattern
+                        else:
+                            merged_seqs[-1].repeat_patterns.update({ch_name:
+                                                                         pattern})
+
+        return merged_seqs
+
     def __repr__(self):
         string_repr = f"####### {self.name} #######\n"
         for seg_name, seg in self.segments.items():
             string_repr += str(seg) + "\n"
         return string_repr
+    
+    def __deepcopy__(self, memo):
+        cls = self.__class__
+        new_seq = cls.__new__(cls)
+        memo[id(self)] = new_seq
+        for k, v in self.__dict__.items():
+            if k == "pulsar": # the reference to pulsar cannot be deepcopied
+                setattr(new_seq, k, v)
+            else:
+                setattr(new_seq, k, deepcopy(v, memo))
+        return new_seq
 
     def plot(self, segments=None, **segment_plot_kwargs):
         """
