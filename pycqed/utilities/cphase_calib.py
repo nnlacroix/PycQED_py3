51285b5eb2a5532be39015414fca44d91946fe91
Stefania Lazar <stefania.lazar@phys.ethz.ch>
2020-06-03 17:44:25 +0200

    Adds get_optimal_amp and plot_and_save_cz_amp_sweep to cphase_calib.py.

--- pycqed/utilities/cphase_calib.py
+++ pycqed/utilities/cphase_calib.py
@@ -1,3 +1,6 @@
+from pycqed.analysis import analysis_toolbox as a_tools
+from pycqed.analysis_v2 import timedomain_analysis as tda
+import matplotlib.pyplot as plt
 import numpy as np
 import lmfit
 
@@ -17,3 +20,85 @@ def update_cz_amplitude(qbc, qbt, phases, amplitudes, target_phase=np.pi,
     print('BEST {} '.format('amplitude'), new_ampl)
     if update:
         qbc.set('upCZ_{}_amplitude'.format(qbt.name), new_ampl)
+
+
+def get_optimal_amp(qbc, qbt, soft_sweep_points, timestamp=None,
+                    classified_ro=False, tangent_fit=False):
+
+    if classified_ro:
+        channel_map = {qb.name: [vn + ' ' +
+                                 qb.instr_uhf() for vn in
+                                 qb.int_avg_classif_det.value_names]
+                       for qb in [qbc, qbt]}
+    else:
+        channel_map = {qb.name: [vn + ' ' +
+                                 qb.instr_uhf() for vn in
+                                 qb.int_avg_det.value_names]
+                       for qb in [qbc, qbt]}
+    tdma = tda.CPhaseLeakageAnalysis(
+        t_start=timestamp,
+        qb_names=[qbc.name, qbt.name],
+        options_dict={'TwoD': True, 'plot_all_traces': False,
+                      'plot_all_probs': False,
+                      'channel_map': channel_map})
+    cphases = tdma.proc_data_dict[
+        'analysis_params_dict']['cphase']['val']
+
+    sweep_pts = list(soft_sweep_points.values())[0]['values']
+    if tangent_fit:
+        fit_res = lmfit.Model(lambda x, m, b: m*np.tan(x/2-np.pi/2) + b).fit(
+            x=cphases, data=sweep_pts,
+            m=(max(sweep_pts)-min(sweep_pts))/((max(cphases)-min(cphases))),
+            b=np.min(sweep_pts))
+    else:
+        fit_res = lmfit.Model(lambda x, m, b: m*x + b).fit(
+            x=cphases, data=sweep_pts,
+            m=(max(sweep_pts)-min(sweep_pts))/((max(cphases)-min(cphases))),
+            b=np.min(sweep_pts))
+    plot_and_save_cz_amp_sweep(cphases=cphases, timestamp=timestamp,
+                               soft_sweep_params_dict=soft_sweep_points,
+                               fit_res=fit_res, save_fig=True, plot_guess=False,
+                               qbc_name=qbc.name, qbt_name=qbt.name)
+    return fit_res
+
+
+def plot_and_save_cz_amp_sweep(cphases, soft_sweep_params_dict, fit_res,
+                               qbc_name, qbt_name, save_fig=True, show=True,
+                               plot_guess=False, timestamp=None):
+
+    sweep_param_name = list(soft_sweep_params_dict)[0]
+    sweep_points = soft_sweep_params_dict[sweep_param_name]['values']
+    unit = soft_sweep_params_dict[sweep_param_name]['unit']
+    best_val = fit_res.model.func(np.pi, **fit_res.best_values)
+    fit_points_init = fit_res.model.func(cphases, **fit_res.init_values)
+    fit_points = fit_res.model.func(cphases, **fit_res.best_values)
+
+    fig, ax = plt.subplots()
+    ax.plot(cphases*180/np.pi, sweep_points, 'o-')
+    ax.plot(cphases*180/np.pi, fit_points, '-r')
+    if plot_guess:
+        ax.plot(cphases*180/np.pi, fit_points_init, '--k')
+    ax.hlines(best_val, cphases[0]*180/np.pi, cphases[-1]*180/np.pi)
+    ax.vlines(180, sweep_points.min(), sweep_points.max())
+    ax.set_ylabel('Flux pulse {} ({})'.format(sweep_param_name, unit))
+    ax.set_xlabel('Conditional phase (rad)')
+    ax.set_title('CZ {}-{}'.format(qbc_name, qbt_name))
+
+    ax.text(0.5, 0.95, 'Best {} = {:.6f} ({})'.format(
+        sweep_param_name, best_val*1e9 if unit=='s' else best_val, unit),
+            horizontalalignment='center', verticalalignment='top',
+            transform=ax.transAxes)
+    if save_fig:
+        import datetime
+        import os
+        fig_title = 'CPhase_amp_sweep_{}_{}'.format(qbc_name, qbt_name)
+        fig_title = '{}--{:%Y%m%d_%H%M%S}'.format(
+            fig_title, datetime.datetime.now())
+        if timestamp is None:
+            save_folder = a_tools.latest_data()
+        else:
+            save_folder = a_tools.get_folder(timestamp)
+        filename = os.path.abspath(os.path.join(save_folder, fig_title+'.png'))
+        fig.savefig(filename, bbox_inches='tight')
+    if show:
+        plt.show()
