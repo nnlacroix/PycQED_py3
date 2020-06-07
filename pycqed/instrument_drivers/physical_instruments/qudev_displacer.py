f2739cbb1a2e1c99dfc84f61067cd5e74795ef90
Graham Norris <gjn@grahamjnorris.com>
2020-06-08 00:07:08 +0200

    Added further functions to NanotecSMI33.

--- pycqed/instrument_drivers/physical_instruments/qudev_displacer.py
+++ pycqed/instrument_drivers/physical_instruments/qudev_displacer.py
@@ -51,6 +51,16 @@ class NanotecSMI33():
     def read_acceleration_ramp(self):
         return np.uint16(self.read_parameter('b'))
 
+    def read_auto_correction_record(self, index=0):
+        """Read the record in controller to use for ramp rates and speeds
+        for the correction run.
+        Index may be between 0 and 32. Indices 1 through 32 refer to
+        record addresses 1 through 32. An index of 0 disables the error
+        correction run and causes an error to be raised if the error
+        correction mode is enabled.
+        """
+        return np.uint8(self.read_parameter('F'))
+
     def read_bldc_current_time_constant(self, time_constant=0):
         """Read current time constant for BLDC motors
         time_constant is defined in ms
@@ -69,18 +79,37 @@ class NanotecSMI33():
     def read_current_record(self):
         return self.read_parameter('|')
 
-    def read_errors(self):
-        for index in range(32):
-            parameter = str(index) + 'E'
-            response = self.read_parameter(parameter)
-            # {0x00: None,
-            #  0x01: 'LOWVOLTAGE',
-            #  0x02: 'TEMP',
-            #  0x04: 'TMC',
-            #  0x08: 'EE',
-            #  0x10: 'QEI',
-            #  0x20: 'INTERNAL',
-            #  }
+    def read_encoder_direction(self):
+        """Read the direction of the rotary encoder.
+        The direction can either be normal or reversed.
+        """
+        states = {
+            0: 'normal',
+            1: 'reversed',
+        }
+        return states[int(self.read_parameter('q'))]
+
+    # def read_errors(self, index):
+    #     for index in range(32):
+    #         parameter = str(index) + 'E'
+    #         response = self.read_parameter(parameter)
+    #         # {0x00: None,
+    #         #  0x01: 'LOWVOLTAGE',
+    #         #  0x02: 'TEMP',
+    #         #  0x04: 'TMC',
+    #         #  0x08: 'EE',
+    #         #  0x10: 'QEI',
+    #         #  0x20: 'INTERNAL',
+    #         #  }
+
+    def read_error_correction_mode(self):
+        """Read the error correcting mode"""
+        modes = {
+            0: 'off',
+            1: 'correction_after_travel',
+            2: 'correction_during_travel',
+        }
+        return modes[int(self.read_parameter('U'))]
 
     def read_firmware_info(self):
         #TODO: parse fields
@@ -88,10 +117,22 @@ class NanotecSMI33():
 
     def read_limit_switch_behavior(self):
         #TODO: parse fields
-        return self.read_parameter('l')
+        return np.uint32(self.read_parameter('l'))
 
     def read_maximum_frequency(self):
         return np.uint32(self.read_parameter('o'))
+
+    def read_motor_address(self):
+        return np.uint8(self.read_paramter('m'))
+
+    def read_motor_id(self):
+        """Read motor ID
+        A motor ID is a uint32 between 0 and 2147483647 that uniquely
+        identifies a motor type and connection type. This motor ID is
+        used by the NanoPro software to automatically set some
+        parameters like phase currents.
+        """
+        return np.uint32(self.read_parameter(':mt'))
 
     def read_motor_type(self, motor_type='stepper'):
         """Sets type of motor"""
@@ -124,7 +165,8 @@ class NanotecSMI33():
     def read_record(self, record=None):
         """Reads record from EEPROM
         if record is None, reads the currently-loaded settings
-        otherwise, if record in range(1,32), reads record from EEPROM"""
+        otherwise, if record in range(1,32), reads record from EEPROM.
+        """
         if record is None:
             return self.read_parameter('|')
         else:
@@ -160,6 +202,16 @@ class NanotecSMI33():
     def read_step_mode(self):
         return int(self.read_parameter('g'))
 
+    def read_swing_out_time(self):
+        """Read the swing out time
+        The swing out time is the settling time between when the
+        controller finishes a run and when it reads the encoder
+        position.
+        The swing_out_time is defined in multiples of 10 ms.
+        This time only matters for error correction runs.
+        """
+        return np.uint8(self.read_parameter('O'))
+
     def read_travel_distance(self):
         return int(self.read_parameter('s'))
 
@@ -168,8 +220,19 @@ class NanotecSMI33():
 
     def set_acceleration_ramp(self, value):
         """Set acceleration ramp
-        Acceleration in Hz/ms = (3000.0/sqrt(float)<parameter>))-11.7)"""
+        Acceleration in Hz/ms = (3000.0/sqrt(float)<parameter>))-11.7)
+        """
         self.write_parameter('b', np.uint16(value))
+
+    def set_auto_correction_record(self, index=0):
+        """Set record in controller to use for ramp rates and speeds
+        for the correction run.
+        Index may be between 0 and 32. Indices 1 through 32 refer to
+        record addresses 1 through 32. An index of 0 disables the error
+        correction run and causes an error to be raised if the error
+        correction mode is enabled.
+        """
+        self.write_parameter('F', str(np.uint8(index)))
 
     def set_bldc_current_time_constant(self, time_constant=0):
         """Set current time constant for BLDC motors
@@ -199,17 +262,39 @@ class NanotecSMI33():
         }
         self.write('|', responses[response])
 
-    def set_error_correction_mode(self, mode):
-        values = {
-            'Off': 0,
-            'CorrectionAfterTravel': 1,
-            'CorrectionDuringTravel': 2
+    def set_encoder_direction(self,state='normal'):
+        """Set the direction of the rotary encoder
+        The direction can either be normal or reversed.
+        """
+        states = {
+            'normal': 0,
+            'reversed': 1,
+        }
+        self.write_parameter('q',str(states[state]))
+
+    def set_error_correction_mode(self, mode='off'):
+        """Set the error correction mode
+        For motors without an encoder, this must be disabled to avoid
+        erroneous correction attempts. correction_after_travel and
+        correction_during_travel both correct errors after the run
+        is completed.
+        """
+        modes = {
+            'off': 0,
+            'correction_after_travel': 1,
+            'correction_during_travel': 2
         }
         if mode not in values.keys():
             #TODO print error message
             pass
-        command = values['mode']
-        self.write_parameter(command)
+        self.write_parameter(modes[mode])
+
+    def set_limit_switch_behavior(self, behavior_mask=17442):
+        """Set the limit switch behavior
+        The behavior is specified as a 16-bit uint32 integer mask.
+        """
+        #TODO: verify that constraints on mask is fulfilled
+        self.write_parameter('l',np.uint32(behavior_mask))
 
     # def set_keyword_parameter(self, keyword, value):
     #     command = keyword + '=' + str(value)
@@ -221,6 +306,21 @@ class NanotecSMI33():
     def set_maximum_frequency(self, frequency):
         """Set maximum frequency (steps per second)"""
         self.write_parameter('o', np.uint32(frequency))
+
+    def set_motor_address(self, address):
+        """Set motor address
+        address must be a uint8 between 1 and 254
+        """
+        self.write_parameter('m', np.uint8(address))
+
+    def set_motor_id(self, id=0):
+        """Set motor ID
+        A motor ID is a uint32 between 0 and 2147483647 that uniquely
+        identifies a motor type and connection type. This motor ID is
+        used by the NanoPro software to automatically set some
+        parameters like phase currents.
+        """
+        self.write_parameter(':mt', np.uint32(id))
 
     def set_motor_type(self, motor_type='stepper'):
         """Sets type of motor"""
@@ -278,6 +378,16 @@ class NanotecSMI33():
             #TODO: value error
             pass
         self.write_parameter('g', value)
+
+    def set_swing_out_time(self, swing_out_time=8):
+        """Set the swing out time
+        The swing out time is the settling time between when the
+        controller finishes a run and when it reads the encoder
+        position.
+        The swing_out_time is defined in multiples of 10 ms.
+        This time only matters for error correction runs.
+        """
+        self.write_parameter('O', np.uint8(swing_out_time))
 
     def set_travel_distance(self, value):
         """Set travel distance in (micro-)steps
