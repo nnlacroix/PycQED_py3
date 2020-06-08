35a75f667b8a03736165c453ad161a108465a665
Nathan Lacroix <lna@student.ethz.ch>
2020-06-08 15:37:31 +0200

    Minor changes for Single Qubit SSRO analysis
    - include additional labels
    - aesthetics

--- pycqed/analysis_v2/readout_analysis.py
+++ pycqed/analysis_v2/readout_analysis.py
@@ -10,7 +10,7 @@ Originally written by Adriaan, updated/rewritten by Rene May 2018
 import itertools
 import logging
 log = logging.getLogger(__name__)
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 from copy import deepcopy
 from pycqed.measurement.calibration_points import CalibrationPoints
 import lmfit
@@ -919,27 +919,8 @@ class Singleshot_Readout_Analysis_Qutrit(ba.BaseDataAnalysis):
             gm.fit(X)
             pred_states = np.argmax(gm.predict_proba(X), axis=1)
 
-            if cov_type == "tied":
-                # in case all components share the same cov mtx return a list
-                # of identical cov matrices
-                covs = [gm.covariances_ for _ in range(gm.n_components)]
-            elif cov_type == "full":
-                # already of the right shape (n_comp, n_features, n_features)
-                covs = gm.covariances_
-            elif cov_type == "spherical":
-                # return list of sigma_i^2 * I instead of list of sigma_i^2
-                covs = [np.diag([gm.covariances_[i]
-                                 for _ in range(X.shape[1])])
-                        for i in range(gm.n_components)]
-            elif cov_type == "diag":
-                # make covariance matrices from diagonals
-                covs = [np.diag(gm.covariances_[i])
-                            for i in range(gm.n_components)]
-            else:
-                raise ValueError("covariance type: {} is not supported"
-                                 .format(cov_type))
             params['means_'] = gm.means_
-            params['covariances_'] = gm.covariances_ #covs
+            params['covariances_'] = gm.covariances_
             params['covariance_type'] = gm.covariance_type
             params['weights_'] = gm.weights_
             params['precisions_cholesky_'] = gm.precisions_cholesky_
@@ -975,7 +956,6 @@ class Singleshot_Readout_Analysis_Qutrit(ba.BaseDataAnalysis):
                                       .format(method, ['ncc', 'gmm',
                                                        'threshold']))
 
-
     @staticmethod
     def fidelity_matrix(prep_states, pred_states, levels=('g', 'e', 'f'),
                         plot=False, normalize=True):
@@ -990,7 +970,7 @@ class Singleshot_Readout_Analysis_Qutrit(ba.BaseDataAnalysis):
     @staticmethod
     def plot_fidelity_matrix(fm, target_names,
                              title="State Assignment Probability Matrix",
-                             auto_shot_info=True,
+                             auto_shot_info=True, ax=None,
                              cmap=None, normalize=True, show=False):
         fidelity_avg = np.trace(fm) / float(np.sum(fm))
         if auto_shot_info:
@@ -998,7 +978,10 @@ class Singleshot_Readout_Analysis_Qutrit(ba.BaseDataAnalysis):
         if cmap is None:
             cmap = plt.get_cmap('Reds')
 
-        fig, ax = plt.subplots(1, figsize=(8, 6))
+        if ax is None:
+            fig, ax = plt.subplots(1, figsize=(8, 6))
+        else:
+            fig = ax.get_figure()
 
         if normalize:
             fm = fm.astype('float') / fm.sum(axis=1)[:, np.newaxis]
@@ -1006,7 +989,8 @@ class Singleshot_Readout_Analysis_Qutrit(ba.BaseDataAnalysis):
         im = ax.imshow(fm, interpolation='nearest', cmap=cmap,
                        norm=mc.LogNorm(), vmin=5e-3, vmax=1.)
         ax.set_title(title)
-        fig.colorbar(im)
+        cb = fig.colorbar(im)
+        cb.set_label('Assignment Probability, $P_{ij}$')
 
         if target_names is not None:
             tick_marks = np.arange(len(target_names))
@@ -1032,7 +1016,6 @@ class Singleshot_Readout_Analysis_Qutrit(ba.BaseDataAnalysis):
         if show:
             plt.show()
         return fig
-
 
     @staticmethod
     def _extract_tree_info(tree_clf, class_names=None):
@@ -1122,35 +1105,50 @@ class Singleshot_Readout_Analysis_Qutrit(ba.BaseDataAnalysis):
         if kwargs.get("fig", None) is None:
             fig, axes = plt.subplots(figsize=(10, 8))
             kwargs['fig'] = fig
-
+        colors = kwargs.get('colors', [None] * len(np.unique(y_true)))
         gs = gridspec.GridSpec(2, 2, width_ratios=[3, 1], height_ratios=[1, 4],
                                figure=kwargs['fig'])
 
         # Create scatter plot
         ax = plt.subplot(gs[1, 0])
-        for yval in np.unique(y_true):
+        for yval, c in zip(np.unique(y_true), colors):
             ax.scatter(data[:, 0][y_true == yval], data[:, 1][y_true == yval],
-                       alpha=kwargs.get('alpha', 0.6), marker='.',
+                       alpha=kwargs.get('alpha', 0.6), marker='.', c=c,
                        label=kwargs.get("legend_labels",
                                         [yval] * len(np.unique(y_true)))[int(yval)])
-        #h, labels = sc.legend_elements()
-        #legend = ax.legend(h, kwargs.get("legend_labels", labels))
-        #ax.add_artist(legend)
+        # h, labels = sc.legend_elements()
+        # legend = ax.legend(h, kwargs.get("legend_labels", labels))
+        # ax.add_artist(legend)
         # Create Y-marginal (right)
-        axr = plt.subplot(gs[1, 1], sharey=ax, frameon=False)
-        for yval in np.unique(y_true):
-            axr.hist(data[:, 1][y_true == yval], bins=50,
+        axr = plt.subplot(gs[1, 1], sharey=ax, frameon=kwargs.get('frameon', True))
+        binr_range = (data[:, 1].min(), data[:, 1].max())
+        for yval, c in zip(np.unique(y_true), colors):
+            axr.hist(data[:, 1][y_true == yval], bins=50, color=c,
+                     range=binr_range,
                      orientation='horizontal', density=False,
                      alpha=kwargs.get('alpha', 0.6))
-        axr.set_xscale(kwargs.get("scale", "log"))
+        axr.set_xscale(kwargs.get("scale", "linear"))
+        axr.set_xlabel("Counts")
+        axr.spines["top"].set_visible(False)
+        axr.spines["right"].set_visible(False)
+        axr.yaxis.set_tick_params(right=False)
+        axr.xaxis.set_tick_params(right=False, top=False)
+
         plt.setp(axr.get_yticklabels(), visible=False)
 
         # Create X-marginal (top)
-        axt = plt.subplot(gs[0, 0], sharex=ax, frameon=False)
+        axt = plt.subplot(gs[0, 0], sharex=ax, frameon=kwargs.get('frameon', True))
+        axt.spines["top"].set_visible(False)
+        axt.spines["right"].set_visible(False)
+        axt.yaxis.set_tick_params(right=False, top=False)
+        axt.xaxis.set_tick_params(right=False, top=False)
+        bint_range = (data[:, 0].min(), data[:, 0].max())
+        axt.set_ylabel("Counts")
         plt.setp(axt.get_xticklabels(), visible=False)
 
-        for yval in np.unique(y_true):
-            axt.hist(data[:, 0][y_true == yval], bins=50,
+        for yval, c in zip(np.unique(y_true), colors):
+            axt.hist(data[:, 0][y_true == yval], bins=50, color=c,
+                     range=bint_range,
                      orientation='vertical', density=False,
                      alpha=kwargs.get('alpha', 0.6))
         axt.set_yscale(kwargs.get("scale", "log"))
@@ -1183,7 +1181,7 @@ class Singleshot_Readout_Analysis_Qutrit(ba.BaseDataAnalysis):
             x_min, x_max = x.min() - margin_x, x.max() + margin_x
             y_min, y_max = y.min() - margin_y, y.max() + margin_y
             if h is None:
-                h = 0.01*(x_max - x_min)
+                h = 0.01 * (x_max - x_min)
             xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                                  np.arange(y_min, y_max, h))
             return xx, yy
@@ -1231,8 +1229,8 @@ class Singleshot_Readout_Analysis_Qutrit(ba.BaseDataAnalysis):
         pearson = cov[0, 1] / np.sqrt(cov[0, 0] * cov[1, 1])
         # Using a special case to obtain the eigenvalues of this
         # two-dimensionl dataset.
-        ell_radius_x = 1#np.sqrt(1 + pearson)
-        ell_radius_y = 1# np.sqrt(1 - pearson)
+        ell_radius_x = np.sqrt(1 + pearson)
+        ell_radius_y = np.sqrt(1 - pearson)
 
         ellipse = Ellipse((0, 0),
                           width=ell_radius_x * 2,
@@ -1258,6 +1256,41 @@ class Singleshot_Readout_Analysis_Qutrit(ba.BaseDataAnalysis):
         ellipse.set_transform(transf + ax.transData)
         return ax.add_patch(ellipse)
 
+    @staticmethod
+    def _get_covariances(gmm, cov_type=None):
+        """
+        Retrieves covariences matrices of each mixture component of GMM
+        depending on the fitting procedure (cov_type)
+        Args:
+            gmm:
+            cov_type:
+
+        Returns:
+
+        """
+        if cov_type is None:
+            cov_type = gmm.covariance_type
+
+        if cov_type == "tied":
+            # in case all components share the same cov mtx return a list
+            # of identical cov matrices
+            covs = [gmm.covariances_ for _ in range(gmm.n_components)]
+        elif cov_type == "full":
+            # already of the right shape (n_comp, n_features, n_features)
+            covs = gmm.covariances_
+        elif cov_type == "spherical":
+            # return list of sigma_i^2 * I instead of list of sigma_i^2
+            covs = [np.diag([gmm.covariances_[i]
+                             for _ in range(gmm.means_.shape[1])])
+                    for i in range(gmm.n_components)]
+        elif cov_type == "diag":
+            # make covariance matrices from diagonals
+            covs = [np.diag(gmm.covariances_[i])
+                    for i in range(gmm.n_components)]
+        else:
+            raise ValueError("covariance type: {} is not supported"
+                             .format(cov_type))
+        return covs
     def prepare_plots(self):
         cmap = plt.get_cmap('tab10')
         tab_x = a_tools.truncate_colormap(cmap, 0, len(self.levels)/10)
