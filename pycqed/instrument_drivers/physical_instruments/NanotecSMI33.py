c955667773ab499b889dfe3672a3208db5407bb2
gnorris <gnorris@student.ethz.ch>
2020-06-10 03:16:23 +0200

    Add input IO buffer flushes to ask

--- pycqed/instrument_drivers/physical_instruments/NanotecSMI33.py
+++ pycqed/instrument_drivers/physical_instruments/NanotecSMI33.py
@@ -813,14 +813,22 @@ class NanotecSMI33(VisaInstrument):
         # Case for 'long' commands
         if cmd[0] == ':' and len(cmd) > 2:
             motor_cmd = self._start_character + self.controller_id + cmd
+            print(motor_cmd)
             motor_response = super().ask(motor_cmd)
+            print(motor_response)
             response = motor_response.lstrip('0').split(cmd)[1].lstrip('=')
+            # Flush the input IO buffer to clear the response from the controller
+            self.visa_handle.flush(pyvisa.constants.VI_IO_IN_BUF)
             return response
         # Case for all other normal commands
         else:
             motor_cmd = self._start_character + self.controller_id + 'Z' + cmd
+            print(motor_cmd)
             motor_response = super().ask(motor_cmd)
+            print(motor_response)
             response = motor_response.lstrip('0').split(cmd)[1].lstrip()
+            # Flush the input IO buffer to clear the response from the controller
+            self.visa_handle.flush(pyvisa.constants.VI_IO_IN_BUF)
             return response
 
 
