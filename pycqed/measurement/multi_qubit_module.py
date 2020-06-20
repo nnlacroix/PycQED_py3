b6ba864fa9b1a222dd8b0197fe1b6bcf7c4b10b8
Christoph Hellings <chellings@phys.ethz.ch>
2020-06-20 12:13:02 +0200

    measure_chevron: added keyword argument compression_seg_lim to enable a compressed 2D sweep

--- pycqed/measurement/multi_qubit_module.py
+++ pycqed/measurement/multi_qubit_module.py
@@ -2560,7 +2560,7 @@ def measure_chevron(dev, qbc, qbt, hard_sweep_params, soft_sweep_params,
                     classified=False, n_cal_points_per_state=1,
                     num_cz_gates=1, cal_states=('g', 'e', 'f'),
                     prep_params=None, exp_metadata=None, analyze=True,
-                    return_seq=False, channels_to_upload=None):
+                    return_seq=False, channels_to_upload=None, **kw):
 
     if isinstance(qbc, str):
         qbc = dev.get_qb(qbc)
@@ -2582,6 +2582,8 @@ def measure_chevron(dev, qbc, qbt, hard_sweep_params, soft_sweep_params,
         log.warning('There is more than one soft sweep parameter.')
     if label is None:
         label = 'Chevron_{}{}'.format(qbc.name, qbt.name)
+    if exp_metadata is None:
+        exp_metadata = {}
     MC = dev.find_instrument('MC')
     for qb in [qbc, qbt]:
         qb.prepare(drive='timedomain')
@@ -2604,6 +2606,12 @@ def measure_chevron(dev, qbc, qbt, hard_sweep_params, soft_sweep_params,
             cz_pulse_name=cz_pulse_name,
             num_cz_gates=num_cz_gates,
             cal_points=cp, upload=False, prep_params=prep_params)
+    # compress 2D sweep
+    if kw.get('compression_seg_lim', None) is not None:
+        sequences, hard_sweep_points, soft_sweep_points, cf = \
+            sequences[0].compress_2D_sweep(sequences,
+                                           kw.get("compression_seg_lim"))
+        exp_metadata.update({'compression_factor': cf})
 
     if return_seq:
         return sequences
@@ -2627,8 +2635,6 @@ def measure_chevron(dev, qbc, qbt, hard_sweep_params, soft_sweep_params,
     MC.set_sweep_points_2D(soft_sweep_points)
     MC.set_detector_function(qbr.int_avg_classif_det if classified
                              else qbr.int_avg_det)
-    if exp_metadata is None:
-        exp_metadata = {}
     exp_metadata.update({'preparation_params': prep_params,
                          'cal_points': repr(cp),
                          'rotate': len(cal_states) != 0,
