42236c0002d78d424bbf28db97a37febce54f8b6
Nathan Lacroix <lna@student.ethz.ch>
2020-06-21 19:27:51 +0200

    Add script for commandline call
    allows to call analysis_daemon.py through the commandline to start an AnalysisDaemon with the corresponding parameters.

--- pycqed/analysis_v2/analysis_daemon.py
+++ pycqed/analysis_v2/analysis_daemon.py
@@ -4,15 +4,16 @@ import time
 import traceback
 import logging
 import os
+import matplotlib.pyplot as plt
 log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
 from pycqed.analysis import analysis_toolbox as a_tools
-a_tools.datadir = 'Q:\\USERS\\nathan\\data\\xld'
 
 class AnalysisDaemon:
     def __init__(self, t_start=None, start=True):
         self.t_start = t_start
         self.last_ts = None
-        self.poll_interval = 5  # seconds
+        self.poll_interval = 10  # seconds
         self.errs = []
         self.job_errs = []
         if start:
@@ -45,14 +46,16 @@ class AnalysisDaemon:
                                                       n_matches=1000)
         except ValueError as e:
             return  # could not find any timestamps matching criteria
-        log.info(f"Searching in: {timestamps}")
+        log.info(f"Searching jobs in: {timestamps[0]} ... {timestamps[-1]}.")
         for folder, ts in zip(folders, timestamps):
             jobs_in_folder = []
             for file in os.listdir(folder):
                 if file.endswith(".job"):
                     jobs_in_folder.append(os.path.join(folder, file))
             if len(jobs_in_folder) > 0:
-                log.info(f"Found {len(jobs_in_folder)} jobs in {ts}")
+                log.info(f"Found {len(jobs_in_folder)} job(s) in {ts}")
+            else:
+                log.info(f"No job found.")
 
             for filename in jobs_in_folder:
                 if os.path.isfile(filename):
@@ -84,9 +87,21 @@ class AnalysisDaemon:
     def run_job(self, job, ts):
         try:
             exec(job)
+            plt.close('all')
         except Exception as e:
             log.error(f"Error in job: {job}:\n{e}")
             self.job_errs.append(traceback.format_exc())
 
 if __name__ == "__main__":
-    ad = AnalysisDaemon(t_start=None)
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data-dir", help="Watch directory")
+    parser.add_argument("--t-start", help="Starting watch time formatted as a "
+                                          "timestamp YYYYmmdd_hhMMss or string"
+                                          " 'now' (default).")
+    args = parser.parse_args()
+    if args.data_dir is not None:
+        a_tools.datadir = args.data_dir
+    if args.t_start == "now":
+        args.t_start = None
+    ad = AnalysisDaemon(t_start=args.t_start)
