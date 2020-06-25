63e11dcedb9423b5f44f728beaf4e08c2db50353
Christoph Hellings <chellings@phys.ethz.ch>
2020-06-25 17:22:07 +0200

    new feature: MWG with LO Calibration The new function mwg_with_lo_calibration_template can turn an existing MWG class into an MWGWithLOCalibration. For an MWG created with MWGWithLOCalibration, QuDev_transmon.prepare can automatically update the I and Q offset as a function of lo frequency. The configuration for this is done in the new parameter ge_lo_leakage_cal of QuDev_Transmon.

---
+++ pycqed/instrument_drivers/meta_instrument/mwg_lo_calibration.py
@@ -0,0 +1,44 @@
+import scipy as sp
+
+from qcodes import validators as vals
+from qcodes.instrument.parameter import ManualParameter
+
+
+def mwg_with_lo_calibration_template(mwg_class):
+    """
+
+    Structure of the calibration data:
+        {name: (param, freqs, cal_vals)}
+
+    """
+
+    class MWGWithLOCalibration(mwg_class):
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+            self.add_parameter('lo_cal_data',
+                               vals=vals.Dict(),
+                               parameter_class=ManualParameter,
+                               initial_value=dict())
+            self.add_parameter('lo_cal_interp_kind',
+                               vals=vals.Enum(
+                                   'linear', 'nearest', 'zero', 'slinear',
+                                   'quadratic', 'cubic', 'previous', 'next'),
+                               parameter_class=ManualParameter,
+                               initial_value='linear')
+
+            self.frequency.set_parser = \
+                lambda val, lo_cal_data=self.lo_cal_data, \
+                       lo_cal_interp_kind=self.lo_cal_interp_kind : \
+                   self.lo_calib(val, lo_cal_data, lo_cal_interp_kind)
+
+        @staticmethod
+        def lo_calib(val, lo_cal_data, lo_cal_interp_kind):
+            for par, freqs, cal_vals in lo_cal_data().values():
+                par(float(sp.interpolate.interp1d(
+                    freqs, cal_vals, kind=lo_cal_interp_kind(),
+                    fill_value=(min(cal_vals), max(cal_vals)))(val)))
+            return val
+
+    return MWGWithLOCalibration
