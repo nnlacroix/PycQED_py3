13997a4665658838e6d2e8d40fd652ce52ddfc28
Ants Remm <remma@phys.ethz.ch>
2020-06-29 14:20:36 +0200

    rever changes not related to hdawg bugfix

--- pycqed/instrument_drivers/physical_instruments/ZurichInstruments/ZI_HDAWG_core.py
+++ pycqed/instrument_drivers/physical_instruments/ZurichInstruments/ZI_HDAWG_core.py
@@ -293,8 +293,8 @@ class ZI_HDAWG_core(zibase.ZI_base_instrument):
                         'message': message
                     }
 
-        if found_errors:
-            raise zibase.ziRuntimeError('Errors detected during run-time!')
+        # if found_errors:
+        #     raise zibase.ziRuntimeError('Errors detected during run-time!')
 
     def clear_errors(self):
         self.seti('raw/error/clear', 1)
