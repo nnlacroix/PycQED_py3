71dcd0ab35562300201fc5a41d1cd2d3fe995e92
Christoph Hellings <chellings@phys.ethz.ch>
2020-07-02 21:30:03 +0200

    First draft of parallel FluxPulseAmplitudeSweep class (based on a new class ParallelLOSweepExperiment)

--- pycqed/measurement/calibration/single_qubit_gates.py
+++ pycqed/measurement/calibration/single_qubit_gates.py
@@ -2,11 +2,14 @@ import numpy as np
 from copy import deepcopy
 import traceback
 from pycqed.measurement.calibration.two_qubit_gates import CalibBuilder
+import pycqed.measurement.sweep_functions as swf
 from pycqed.measurement.waveform_control.block import Block, ParametricValue
 from pycqed.measurement.sweep_points import SweepPoints
 from pycqed.analysis import fitting_models as fit_mods
 import pycqed.analysis_v2.timedomain_analysis as tda
+from pycqed.utilities.general import temporary_value
 import logging
+
 log = logging.getLogger(__name__)
 
 
@@ -200,3 +203,143 @@ class T1FrequencySweep(CalibBuilder):
         self.analysis = tda.T1FrequencySweepAnalysis(
             qb_names=self.meas_obj_names,
             options_dict=dict(TwoD=False, all_fits=self.all_fits))
+
+
+class ParallelLOSweepExperiment(CalibBuilder):
+    def __init__(self, task_list, sweep_points=None, **kw):
+        for task in task_list:
+            if not isinstance(task['qb'], str):
+                task['qb'] = task['qb'].name
+            if not 'prefix' in task:
+                task['prefix'] = f"{task['qb']}_"
+
+        super().__init__(task_list, **kw)
+        self.lo_offsets = {}
+        self.lo_sweep_points = []
+        self.analysis = {}
+
+        preprocessed_task_list = self.preprocess_task_list(sweep_points)
+        self.resolve_lo_sweep_points(**kw)
+        self.sequences, self.mc_points = self.parallel_sweep(
+            preprocessed_task_list, self.sweep_block, **kw)
+
+    def resolve_lo_sweep_points(self, freq_sp_suffix='freq', **kw):
+        all_freqs = self.sweep_points.get_sweep_params_property('values', 1,
+                                                                'all')
+        if np.ndim(all_freqs) == 1:
+            all_freqs = [all_freqs]
+        all_diffs = [np.diff(freqs) for freqs in all_freqs]
+        assert all([sum(abs(diff - all_diffs[0])) == 0 for diff in
+                    all_diffs]), "The steps between frequency sweep points " \
+                                 "must be the same for all qubits."
+        self.lo_sweep_points = all_freqs[0] - all_freqs[0][0]
+
+        if self.qubits is None:
+            log.warning('No qubit objects provided. Creating the sequence '
+                        'without checking for ge_mod_freq corrections.')
+        else:
+            temp_vals = []
+            for task in self.task_list:
+                qb = self.get_qubits(task['qb'])[0][0]
+                sp = self.exp_metadata['meas_obj_sweep_points_map'][qb.name]
+                freq_sp = [s for s in sp if s.endswith(freq_sp_suffix)][0]
+                f_start = self.sweep_points.get_sweep_params_property(
+                    'values', 1, freq_sp)[0]
+                lo = qb.instr_ge_lo.get_instr()
+                if lo not in self.lo_offsets:
+                    self.lo_offsets[lo] = f_start - qb.ge_mod_freq()
+                else:
+                    temp_vals.append(
+                        (qb.ge_mod_freq, f_start - self.lo_offsets[lo]))
+
+            with temporary_value(*temp_vals):
+                self.update_operation_dict()
+
+    def run_measurement(self, **kw):
+        name = 'Drive frequency shift'
+        sweep_functions = [swf.Offset_Sweep(
+            lo.frequency, offset, name=name, parameter_name=name, unit='Hz')
+            for lo, offset in self.lo_offsets.items()]
+        self.sweep_functions = [
+            self.sweep_functions[0], swf.multi_sweep_function(
+                sweep_functions, name=name, parameter_name=name)]
+        self.mc_points[1] = self.lo_sweep_points
+        super().run_measurement(**kw)
+
+    def get_meas_objs_from_task(self, task):
+        return [task['qb']]
+
+    def sweep_block(self, **kw):
+        raise NotImplementedError('Child class has to implement sweep_block.')
+
+
+class FluxPulseAmplitudeSweep(ParallelLOSweepExperiment):
+    """
+        Flux pulse amplitude measurement used to determine the qubits energy in
+        dependence of flux pulse amplitude.
+
+        pulse sequence:
+           |    -------------    |X180|  ---------------------  |RO|
+           |    ---   | ---- fluxpulse ----- |
+
+
+            sweep_points:
+            amplitude (numpy array): array of amplitudes of the flux pulse
+            freq (numpy array): array of drive frequencies
+
+        Returns: None
+
+    """
+
+    def __init__(self, task_list, sweep_points=None, **kw):
+        try:
+            self.experiment_name = 'Flux_amplitude'
+            super().__init__(task_list, sweep_points, **kw)
+            self.exp_metadata.update({"global_PCA": True})
+            self.autorun(**kw)
+
+        except Exception as x:
+            self.exception = x
+            traceback.print_exc()
+
+    def sweep_block(self, qb, flux_op_code=None, delay=None,
+                                  **kw):
+        """
+        Performs X180 pulse on top of a fluxpulse
+        :param qb: (str) the name of the qubit
+        :param flux_op_code: (optional str) the flux pulse op_code (default:
+            FP qb)
+        :param delay: (optional float): flux pulse delay (default: centered to
+            center of drive pulse)
+        :param kw:
+        """
+        if flux_op_code is None:
+            flux_op_code = f'FP {qb}'
+        pulse_modifs = {'X180': {'element_name': 'FPA_Pi_el'}}
+        b = self.block_from_ops(f'ge_flux {qb}',
+                                 [f'X180 {qb}', flux_op_code],
+                                 pulse_modifs=pulse_modifs)
+        fp = b.pulses[1]
+        fp['ref_point'] = 'middle'
+        if delay is None:
+            delay = fp['pulse_length'] / 2
+        fp['pulse_delay'] = -fp.get('buffer_length_start', 0) - delay
+        fp['amplitude'] = ParametricValue('amplitude')
+        return b
+
+    def run_analysis(self, **kw):
+        """
+        Runs analysis and stores analysis instances in self.analysis.
+        :param kw:
+        """
+        for task in self.task_list:
+            qb_name = task['qb']
+            self.analysis[qb_name] = tda.FluxAmplitudeSweepAnalysis(
+                qb_names=[qb_name], options_dict=dict(TwoD=True))
+
+        if self.update:
+            for qb in self.qubits:
+                if qb.name in self.analysis:
+                    qb.fit_ge_freq_from_flux_pulse_amp(
+                        self.analysis[qb.name].fit_res[
+                            f'freq_fit_{qb.name}'].best_values)
