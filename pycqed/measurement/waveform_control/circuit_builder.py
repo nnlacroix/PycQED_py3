96bf1f8ecee8b68caa2d06a2b98386ce170f97f0
Christoph Hellings <chellings@phys.ethz.ch>
2020-07-02 21:03:31 +0200

    CircuitBuilder: new method update_operation_dict

--- pycqed/measurement/waveform_control/circuit_builder.py
+++ pycqed/measurement/waveform_control/circuit_builder.py
@@ -29,14 +29,7 @@ class CircuitBuilder:
         self.dev = dev
         self.qubits, self.qb_names = self.extract_qubits(
             dev, qubits, operation_dict, filter_qb_names)
-
-        if operation_dict is not None:
-            self.operation_dict = deepcopy(operation_dict)
-        elif dev is not None:
-            self.operation_dict = deepcopy(dev.get_operation_dict())
-        else:
-            self.operation_dict = deepcopy(mqm.get_operation_dict(qubits))
-
+        self.update_operation_dict(operation_dict)
         self.cz_pulse_name = kw.get('cz_pulse_name', 'upCZ')
         self.prep_params = kw.get('prep_params', None)
 
@@ -67,6 +60,22 @@ class CircuitBuilder:
                 qubits = [qb for qb in qubits if qb.name in filter_qb_names]
             qb_names = [qb for qb in qb_names if qb in filter_qb_names]
         return qubits, qb_names
+
+    def update_operation_dict(self, operation_dict=None):
+        """
+        Updates the stored operation_dict based on the passed operation_dict or
+        based on the stored device/qubit objects.
+        :param operation_dict: (optional) The operation dict to be used. If
+            not provided, an operation dict is generated from  the stored
+            device/qubit objects.
+        :return:
+        """
+        if operation_dict is not None:
+            self.operation_dict = deepcopy(operation_dict)
+        elif self.dev is not None:
+            self.operation_dict = deepcopy(self.dev.get_operation_dict())
+        else:
+            self.operation_dict = deepcopy(mqm.get_operation_dict(self.qubits))
 
     def get_qubits(self, qb_names=None):
         """
