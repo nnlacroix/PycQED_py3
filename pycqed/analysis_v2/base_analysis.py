40dcf790d48f3ce37953b052f7b42da8bb055be0
Christoph Hellings <chellings@phys.ethz.ch>
2020-07-10 15:38:03 +0200

    BaseDataAnalysis: make sure that raw_data_dict['hard_sweep_points'] is set in all cases

--- pycqed/analysis_v2/base_analysis.py
+++ pycqed/analysis_v2/base_analysis.py
@@ -443,6 +443,7 @@ class BaseDataAnalysis(object):
             # run in 1D mode (so only 1 column of sweep points in hdf5 file)
             # CURRENTLY ONLY WORKS WITH SweepPoints CLASS INSTANCES
             hybrid_measurement = False
+            raw_data_dict['hard_sweep_points'] = np.unique(mc_points[0])
             if mc_points.shape[0] > 1:
                 hsp = np.unique(mc_points[0])
                 ssp = np.unique(mc_points[1:])
@@ -473,8 +474,6 @@ class BaseDataAnalysis(object):
                     ssp = np.arange(len(dim_2_sp))
                     raw_data_dict['hard_sweep_points'] = hsp
                     raw_data_dict['soft_sweep_points'] = ssp
-            else:
-                raw_data_dict['hard_sweep_points'] = np.unique(mc_points[0])
 
             data = measured_data[-len(value_names):]
             if data.shape[0] != len(value_names):
