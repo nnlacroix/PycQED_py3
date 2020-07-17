afe1fc819f3ec3fad54fb629541d204b0c0b4ce0
Christoph Hellings <chellings@phys.ethz.ch>
2020-07-17 10:13:44 +0200

    measure_chevron: Revert unneeded cal_states_rotations in metadata

--- pycqed/measurement/multi_qubit_module.py
+++ pycqed/measurement/multi_qubit_module.py
@@ -2502,9 +2502,6 @@ def measure_chevron(dev, qbc, qbt, hard_sweep_params, soft_sweep_params,
     exp_metadata.update({
         'preparation_params': prep_params,
         'cal_points': repr(cp),
-        'cal_states_rotations':
-             {qbr.name: {'g': 0, 'e': 1, 'f': 2}} if
-             (len(cal_states) != 0 and not classified) else None,
         'rotate': len(cal_states) != 0,
         'data_to_fit': {qbr.name: 'pe'},
         'sweep_points': sweep_points,
