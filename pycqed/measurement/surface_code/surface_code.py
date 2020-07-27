8dd0525cc348d7494010c385e82392e132f0728b
Ants Remm <remma@phys.ethz.ch>
2020-07-27 20:51:00 +0200

    implementation of the main block of a surface code experiment
    remaining issues to be solved:
    * add final readout and initial state prep options
    * add option for dummy readout pulses
    * investigate what happens on other data qubit vawgs during DIO elements (something weird)
    * make sure that enforce_single_element doesn't change the element of the cancellation pulses on other AWGs

--- pycqed/measurement/surface_code/surface_code.py
+++ pycqed/measurement/surface_code/surface_code.py
@@ -1,11 +1,19 @@
 
 import pycqed.measurement.quantum_experiment as qe
-
+import pycqed.measurement.waveform_control.block as block_mod
 
 class SurfaceCodeExperiment(qe.QuantumExperiment):
-    def __init__(self, data_qubits, ancilla_qubits, parity_maps,
-                 readout_round_lengths, nr_cycles, initializations,
-                 finalizations, ancilla_dd_pulse='Y180', **kw):
+    block_align = None
+    type_to_ops_map = {
+        'X': ('Y90', 'mY90'),
+        'Y': ('mX90', 'X90'),
+        'Z': (None, None),
+    }
+
+    def __init__(self, data_qubits, ancilla_qubits,
+                 readout_rounds, nr_cycles, initializations,
+                 finalizations, ancilla_reset=False,
+                 ancilla_dd=True, **kw):
         # provide default values
         for k, v in [
             ('qubits', data_qubits + ancilla_qubits),
@@ -16,12 +24,14 @@ class SurfaceCodeExperiment(qe.QuantumExperiment):
         super().__init__(**kw)
         self.data_qubits = data_qubits
         self.ancilla_qubits = ancilla_qubits
-        self.parity_maps = parity_maps
-        self.readout_round_lengths = readout_round_lengths
+        self.readout_rounds = readout_rounds
+        self.ancilla_reset = ancilla_reset
         self.nr_cycles = nr_cycles
         self.initializations = initializations
         self.finalizations = finalizations
-        self.ancilla_dd_pulse = ancilla_dd_pulse
+        self.ancilla_dd = ancilla_dd
+        self.cycle_length = sum([r['round_length']
+                                 for r in self.readout_rounds])
 
     def main_block(self):
         """Block tree of the experiment:
@@ -34,8 +44,15 @@ class SurfaceCodeExperiment(qe.QuantumExperiment):
                       and dynamical decouplings
                 - The ancilla readout blocks, one for each readout round.
         """
+        pulses = []
+        for cycle in range(self.nr_cycles):
+            c = self._cycle_block(cycle)
+            pulses += c.build(ref_pulse='start',
+                              block_delay=cycle*self.cycle_length)
+        return block_mod.Block('main', pulses)
 
-    def _parallel_cz_step_block(self, gates, dd_qubits=None, dd_pulse='Y180'):
+    def _parallel_cz_step_block(self, gates, dd_qubits=None, dd_pulse='X180',
+                                pulse_modifs=None):
         """
         Creates a block containing parallel CZ gates and dynamical decoupling
         pulses. For now all CZ gates are aligned at the start and the dd pulses
@@ -48,37 +65,114 @@ class SurfaceCodeExperiment(qe.QuantumExperiment):
         Returns:
             A block containing the relevant pulses.
         """
+        dd_qubits = dd_qubits if dd_qubits is not None else []
         block_name = 'CZ_' + '_'.join([''.join(gate) for gate in gates])
         ops = ['CZ {} {}'.format(*gate) for gate in gates]
         ops += [dd_pulse + ' ' + dd_qb for dd_qb in dd_qubits]
-        pulse_modifs = {}
-        pulse_modifs.update({
-            idx: {'ref_point': 'start', 'ref_point_new': 'start'}
-            for idx in range(1, len(gates))})
-        pulse_modifs.update({
-            idx: {'ref_point': 'center', 'ref_point_new': 'center'}
-            for idx in range(len(gates), len(ops))})
-        return self.block_from_ops(block_name, ops, pulse_modifs=pulse_modifs)
+        blocks = [self.block_from_ops(op, [op], pulse_modifs=pulse_modifs)
+                  for op in ops]
+        return self.simultaneous_blocks(block_name, blocks,
+                                        block_align=self.block_align)
 
-    def _readout_round_gates_block(self, readout_round):
-        round_parity_maps = [
-            pm for pm in self.parity_maps if pm['round'] == readout_round]
+    def _readout_round_gates_block(self, readout_round, cycle=0):
+        element_name = f'parity_maps_{readout_round}_{cycle}'
+        pulse_modifs = {'all': dict(element_name=element_name)}
+
+        round_parity_maps = self.readout_rounds[readout_round]['parity_maps']
         ancilla_steps = {pm['ancilla']: pm['data'] for pm in round_parity_maps}
         total_steps = max([len(v) for v in ancilla_steps.values()])
+        if total_steps % 2:
+            raise NotImplementedError('Code has not been written for odd weight'
+                                      ' parity maps.')
         ancilla_steps = {a: (total_steps - len(v)) // 2 * [None] +
                             list(v) + (total_steps - len(v) + 1) // 2 * [None]
                          for a, v in ancilla_steps.items()}
+        # cz gates
         gate_lists = [
             [(a, ds[s]) for a, ds in ancilla_steps.items() if ds[s] is not None]
             for s in range(total_steps)]
-        cz_step_blocks = [self._parallel_cz_step_block(gates)
-                          for gates in gate_lists]
-        if self.ancilla_dd_pulse is not None:
-            ancilla_dd_block = self.block_from_ops(
-                'ancilla_dd_block',
-                [self.ancilla_dd_pulse + ' ' + a for a in ancilla_steps],
-                pulse_modifs={'all': {'ref_pulse': 'start'}})
-            cz_step_blocks = cz_step_blocks[:len(total_steps)//2] + \
-                [ancilla_dd_block] + cz_step_blocks[len(total_steps)//2:]
+        cz_step_blocks = [
+            self._parallel_cz_step_block(gates, pulse_modifs=pulse_modifs)
+            for gates in gate_lists]
 
-        # figure out
+        # ancilla dd
+        if self.ancilla_dd:
+            ops = []
+            for a, ds in ancilla_steps.items():
+                ops += [f'Y180 {a}', f'Z180 {a}']
+                ops += [f'Z180 {d}' for d in ds[total_steps//2:]]
+
+            blocks = [self.block_from_ops(op, [op], pulse_modifs=pulse_modifs)
+                      for op in ops]
+            ancilla_dd_block = self.simultaneous_blocks(
+                'ancilla_dd_block', blocks, block_align=self.block_align)
+            cz_step_blocks = cz_step_blocks[:total_steps//2] + \
+                [ancilla_dd_block] + cz_step_blocks[total_steps//2:]
+
+        # data qubit and ancilla basis changes
+        qubit_bases = {}
+        for pm in round_parity_maps:
+            assert qubit_bases.get(pm['ancilla'], 'X') == 'X'
+            qubit_bases[pm['ancilla']] = 'X'
+            for qb in pm['data']:
+                assert qubit_bases.get(qb, pm['type']) == pm['type']
+                qubit_bases[qb] = pm['type']
+        ops_init = []
+        ops_final = []
+        for qb, basis in qubit_bases.items():
+            basis = self.type_to_ops_map.get(basis, basis)
+            assert isinstance(basis, tuple)
+            if basis[0] is not None:
+                ops_init += [f'{basis[0]} {qb}']
+            if basis[1] is not None:
+                ops_final += [f'{basis[1]} {qb}']
+        blocks = [self.block_from_ops(op, [op], pulse_modifs=pulse_modifs)
+                  for op in ops_init]
+        init_block = self.simultaneous_blocks('init_block', blocks,
+                                              block_align=self.block_align)
+        blocks = [self.block_from_ops(op, [op], pulse_modifs=pulse_modifs)
+                  for op in ops_final]
+        final_block = self.simultaneous_blocks('final_block', blocks,
+                                               block_align=self.block_align)
+        blocks = [init_block] + cz_step_blocks + [final_block]
+
+        return self.sequential_blocks(element_name, blocks)
+
+    def _readout_round_readout_block(self, readout_round, cycle=0):
+        round_parity_maps = self.readout_rounds[readout_round]['parity_maps']
+        ancillas = [pm['ancilla'] for pm in round_parity_maps]
+
+        element_name = f'readouts_{readout_round}_{cycle}'
+        pulse_modifs = {'all': dict(element_name=element_name,
+                                    ref_point='start')}
+        ro_block = self.block_from_ops(element_name,
+                                       [f'RO {a}' for a in ancillas],
+                                       pulse_modifs=pulse_modifs)
+
+        element_name = f'resets_{readout_round}_{cycle}'
+        pulse_modifs = {i: dict(element_name=element_name,
+                                codeword=i//len(ancillas),
+                                ref_point_new='end')
+                        for i in range(2*len(ancillas))}
+        ops = [f'I {a}' for a in ancillas] + [f'X180 {a}' for a in ancillas]
+        reset_block = self.block_from_ops(element_name, ops,
+                                          pulse_modifs=pulse_modifs)
+
+        return ro_block, reset_block
+
+    def _cycle_block(self, cycle=0):
+        pulses = []
+        round_delay = 0
+        for readout_round, readout_round_pars in enumerate(self.readout_rounds):
+            g = self._readout_round_gates_block(readout_round, cycle)
+            r, i = self._readout_round_readout_block(readout_round, cycle)
+            pulses += g.build(ref_pulse='start', block_delay=round_delay)
+            ro_name = f'readouts_{readout_round}_{cycle}'
+            pulses += r.build(name=ro_name)
+            round_delay += readout_round_pars['round_length']
+            if self.ancilla_reset:
+                pulses += i.build(
+                    ref_pulse=ro_name+'-|-start',
+                    block_delay=readout_round_pars['reset_delay'])
+        return block_mod.Block(f'cycle_{cycle}', pulses)
+
