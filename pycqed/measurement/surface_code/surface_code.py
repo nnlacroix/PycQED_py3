2676d6e4a2ed991b971d9b8790e155c655afb016
Ants Remm <remma@phys.ethz.ch>
2020-07-28 14:04:24 +0200

    add more options to SurfaceCodeExperiment add proper initializations and finalizations

--- pycqed/measurement/surface_code/surface_code.py
+++ pycqed/measurement/surface_code/surface_code.py
@@ -1,9 +1,11 @@
-
-import pycqed.measurement.quantum_experiment as qe
+import itertools
+import pycqed.measurement.quantum_experiment as qe_mod
 import pycqed.measurement.waveform_control.block as block_mod
+import pycqed.measurement.calibration.calibration_points as cp_mod
+from pycqed.measurement import sweep_points as sp_mod
 
-class SurfaceCodeExperiment(qe.QuantumExperiment):
-    block_align = None
+class SurfaceCodeExperiment(qe_mod.QuantumExperiment):
+    block_align = 'center'
     type_to_ops_map = {
         'X': ('Y90', 'mY90'),
         'Y': ('mX90', 'X90'),
@@ -11,9 +13,10 @@ class SurfaceCodeExperiment(qe.QuantumExperiment):
     }
 
     def __init__(self, data_qubits, ancilla_qubits,
-                 readout_rounds, nr_cycles, initializations,
-                 finalizations, ancilla_reset=False,
-                 ancilla_dd=True, **kw):
+                 readout_rounds, nr_cycles, initializations=None,
+                 finalizations=None, ancilla_reset=False,
+                 ancilla_dd=True, skip_last_ancilla_readout=False,
+                 **kw):
         # provide default values
         for k, v in [
             ('qubits', data_qubits + ancilla_qubits),
@@ -32,9 +35,53 @@ class SurfaceCodeExperiment(qe.QuantumExperiment):
         self.ancilla_dd = ancilla_dd
         self.cycle_length = sum([r['round_length']
                                  for r in self.readout_rounds])
+        self.skip_last_ancilla_readout = skip_last_ancilla_readout
+        self.final_readout_delay = kw.get('final_readout_delay', 0)
+        self._parse_initializations()
+        self.parse_finalizations(
+            basis_rots=kw.get('basis_rots', ('I', 'X90', 'Y90')))
+        self.sweep_points = sp_mod.SweepPoints()
+        self.sweep_points.add_sweep_parameter(
+            'finalize', self.finalizations, '', 'Final', dimension=0)
+        self.sweep_points.add_sweep_parameter(
+            'initialize', self.initializations, '', 'Init', dimension=1)
+        self.sequences, self.mc_points = self.sweep_n_dim(
+            self.sweep_points, self.main_block())
+
+    def parse_finalizations(self, basis_rots):
+        if self.finalizations is None or self.finalizations == 'logical_z':
+            self.finalizations = [len(self.data_qubits) * ['I']]
+        elif self.finalizations == 'logical_x':
+            self.finalizations = [len(self.data_qubits) * ['Y90']]
+        elif self.finalizations == 'data_tomo' or self.finalizations == 'tomo':
+            self.finalizations = self.tomography_pulses(
+                [q.name for q in self.data_qubits], basis_rots)
+        elif self.finalizations == 'full_tomo':
+            self.finalizations = self.tomography_pulses(
+                [q.name for q in self.qubits], basis_rots)
+        for i in range(len(self.finalizations)):
+            fin = list(self.finalizations[i])
+            if len(fin) < len(self.qubits):
+                fin += (len(self.qubits) - len(fin)) * ['I']
+            self.finalizations[i] = fin[:len(self.qubits)]
+
+    def _parse_initializations(self):
+        if self.initializations is None:
+            self.initializations = [len(self.qubits) * '0']
+        elif self.initializations == 'full_data_z':
+            self.initializations = list(
+                itertools.product(['0', '1'], repeat=len(self.data_qubits)))
+        elif self.initializations == 'full_data_x':
+            self.initializations = list(
+                itertools.product(['+', '-'], repeat=len(self.data_qubits)))
+        for i in range(len(self.initializations)):
+            init = list(self.initializations[i])
+            if len(init) < len(self.qubits):
+                init += (len(self.qubits) - len(init)) * ['0']
+            self.initializations[i] = init[:len(self.qubits)]
 
     def main_block(self):
-        """Block tree of the experiment:
+        """Block structure of the experiment:
         * Full experiment block excluding init. and final (tomo) readout
             o `nr_cycles` copies of the cycle block
                 - Gates block for each readout round. The length is explicitly
@@ -49,7 +96,8 @@ class SurfaceCodeExperiment(qe.QuantumExperiment):
             c = self._cycle_block(cycle)
             pulses += c.build(ref_pulse='start',
                               block_delay=cycle*self.cycle_length)
-        return block_mod.Block('main', pulses)
+        return block_mod.Block(
+            'main', pulses, block_end={'pulse_delay': self.final_readout_delay})
 
     def _parallel_cz_step_block(self, gates, dd_qubits=None, dd_pulse='X180',
                                 pulse_modifs=None):
@@ -75,8 +123,7 @@ class SurfaceCodeExperiment(qe.QuantumExperiment):
                                         block_align=self.block_align)
 
     def _readout_round_gates_block(self, readout_round, cycle=0):
-        element_name = f'parity_maps_{readout_round}_{cycle}'
-        pulse_modifs = {'all': dict(element_name=element_name)}
+
 
         round_parity_maps = self.readout_rounds[readout_round]['parity_maps']
         ancilla_steps = {pm['ancilla']: pm['data'] for pm in round_parity_maps}
@@ -88,6 +135,8 @@ class SurfaceCodeExperiment(qe.QuantumExperiment):
                             list(v) + (total_steps - len(v) + 1) // 2 * [None]
                          for a, v in ancilla_steps.items()}
         # cz gates
+        element_name = f'parity_map_entangle_{readout_round}_{cycle}'
+        pulse_modifs = {'all': dict(element_name=element_name)}
         gate_lists = [
             [(a, ds[s]) for a, ds in ancilla_steps.items() if ds[s] is not None]
             for s in range(total_steps)]
@@ -96,6 +145,8 @@ class SurfaceCodeExperiment(qe.QuantumExperiment):
             for gates in gate_lists]
 
         # ancilla dd
+        element_name = f'parity_map_ancilla_dd_{readout_round}_{cycle}'
+        pulse_modifs = {'all': dict(element_name=element_name)}
         if self.ancilla_dd:
             ops = []
             for a, ds in ancilla_steps.items():
@@ -126,10 +177,14 @@ class SurfaceCodeExperiment(qe.QuantumExperiment):
                 ops_init += [f'{basis[0]} {qb}']
             if basis[1] is not None:
                 ops_final += [f'{basis[1]} {qb}']
+        element_name = f'parity_basis_init_{readout_round}_{cycle}'
+        pulse_modifs = {'all': dict(element_name=element_name)}
         blocks = [self.block_from_ops(op, [op], pulse_modifs=pulse_modifs)
                   for op in ops_init]
         init_block = self.simultaneous_blocks('init_block', blocks,
                                               block_align=self.block_align)
+        element_name = f'parity_basis_final_{readout_round}_{cycle}'
+        pulse_modifs = {'all': dict(element_name=element_name)}
         blocks = [self.block_from_ops(op, [op], pulse_modifs=pulse_modifs)
                   for op in ops_final]
         final_block = self.simultaneous_blocks('final_block', blocks,
@@ -167,6 +222,9 @@ class SurfaceCodeExperiment(qe.QuantumExperiment):
             g = self._readout_round_gates_block(readout_round, cycle)
             r, i = self._readout_round_readout_block(readout_round, cycle)
             pulses += g.build(ref_pulse='start', block_delay=round_delay)
+            if self.skip_last_ancilla_readout and cycle == self.nr_cycles - 1 \
+                    and readout_round == len(self.readout_rounds) - 1:
+                continue
             ro_name = f'readouts_{readout_round}_{cycle}'
             pulses += r.build(name=ro_name)
             round_delay += readout_round_pars['round_length']
