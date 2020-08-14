9da4ebea9e55aa33fa3e2096d735c33107733b6d
Stefania Lazar <stefania.lazar@phys.ethz.ch>
2020-08-14 11:06:05 +0200

    Adds __init__.py to analysis_v3.

---
+++ pycqed/analysis_v3/__init__.py
@@ -0,0 +1,35 @@
+# This file initializes all the node modules in analysis_v3.
+
+# pipeline_analysis.py must be initialized first in order to create the global
+# variable search_modules, a set with all modules in analysis_v3 that contain
+# node functions. Each of these modules will populate search_modules with
+# the functions inside the module.
+
+from pycqed.analysis_v3 import processing_pipeline as pp_mod
+from pycqed.analysis_v3 import pipeline_analysis as pla
+from pycqed.analysis_v3 import helper_functions as hlp_mod
+from pycqed.analysis_v3 import data_extraction as dat_extr_mod
+from pycqed.analysis_v3 import fitting as fit_mod
+from pycqed.analysis_v3 import plotting as plot_mod
+from pycqed.analysis_v3 import saving as save_mod
+from pycqed.analysis_v3 import data_processing as dat_proc_mod
+from pycqed.analysis_v3 import rabi_analysis as rabi_ana
+from pycqed.analysis_v3 import ramsey_analysis as ramsey_ana
+from pycqed.analysis_v3 import randomized_benchmarking_analysis as rb_ana
+from pycqed.analysis_v3 import tomography_analysis as tomo_ana
+
+
+from importlib import reload
+def reload_anav3():
+    reload(pp_mod)
+    reload(pla)
+    reload(hlp_mod)
+    reload(dat_extr_mod)
+    reload(fit_mod)
+    reload(plot_mod)
+    reload(save_mod)
+    reload(dat_proc_mod)
+    reload(rabi_ana)
+    reload(ramsey_ana)
+    reload(rb_ana)
+    reload(tomo_ana)
