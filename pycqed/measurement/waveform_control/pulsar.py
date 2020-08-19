0f027f10ddadb331af43d5a3957ac470307b7cb6
Christoph Hellings <chellings@phys.ethz.ch>
2020-08-19 21:17:25 +0200

    flux crosstalk cancellation: store flux_crosstalk_cancellation_mtx in pulsar instead of flux_crosstalk_mtx

--- pycqed/measurement/waveform_control/pulsar.py
+++ pycqed/measurement/waveform_control/pulsar.py
@@ -1072,8 +1072,8 @@ class Pulsar(AWG5014Pulsar, HDAWG8Pulsar, UHFQCPulsar, Instrument):
                            parameter_class=ManualParameter, vals=vals.Bool())
         self.add_parameter('flux_channels', initial_value=[],
                            parameter_class=ManualParameter, vals=vals.Lists())
-        self.add_parameter('flux_crosstalk_mtx', initial_value=None,
-                           parameter_class=ManualParameter)
+        self.add_parameter('flux_crosstalk_cancellation_mtx',
+                           initial_value=None, parameter_class=ManualParameter)
 
         self._inter_element_spacing = 'auto'
         self.channels = set() # channel names
