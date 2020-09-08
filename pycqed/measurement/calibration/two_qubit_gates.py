3a26466e3a44d11221cf695a8b2f876cd59d82ff
Stefania Lazar <stefania.lazar@phys.ethz.ch>
2020-09-08 09:09:47 +0200

    Improves add_default_ramsey_sweep_points to allow both tiling and repeat, or to not do either.

--- pycqed/measurement/calibration/two_qubit_gates.py
+++ pycqed/measurement/calibration/two_qubit_gates.py
@@ -622,7 +622,8 @@ class CalibBuilder(MultiTaskingExperiment):
         return Block('prepend', prepend_pulses)
 
     @staticmethod
-    def add_default_ramsey_sweep_points(sweep_points, **kw):
+    def add_default_ramsey_sweep_points(sweep_points, tile=2,
+                                        repeat=0, **kw):
         """
         Adds phase sweep points for Ramsey-type experiments to the provided
         sweep_points. Assumes that each phase is required twice (to measure a
@@ -631,6 +632,8 @@ class CalibBuilder(MultiTaskingExperiment):
 
         :param sweep_points: (SweepPoints object, list of dicts, or None) the
             existing sweep points
+        :param tile: (int) TODO, default: 2
+        :param repeat: (int) TODO, default: 0
         :param kw: keyword arguments
             nr_phases: how many phase sweep points should be added, default: 6.
                 If there already exist sweep points in dimension 0, this
@@ -640,6 +643,8 @@ class CalibBuilder(MultiTaskingExperiment):
         """
         # ensure that sweep_points is a SweepPoints object with at least two
         # dimensions
+        if tile > 0 and repeat > 0:
+            raise ValueError('"repeat" and "tile" cannot both be > 0.')
         sweep_points = SweepPoints(from_dict_list=sweep_points, min_length=2)
         # If there already exist sweep points in dimension 0, this adapt the
         # number of phases to the number of existing sweep points.
@@ -650,10 +655,12 @@ class CalibBuilder(MultiTaskingExperiment):
         # create the phase sweep points (with each phase twice)
         hard_sweep_dict = SweepPoints()
         if 'phase' not in sweep_points[0]:
-            hard_sweep_dict.add_sweep_parameter(
-                'phase',
-                np.tile(np.linspace(0, 2 * np.pi, nr_phases) * 180 / np.pi, 2),
-                'deg')
+            phases = np.linspace(0, 2 * np.pi, nr_phases) * 180 / np.pi
+            if tile > 0:
+                phases = np.tile(phases, tile)
+            elif repeat > 0:
+                phases = np.repeat(phases, repeat)
+            hard_sweep_dict.add_sweep_parameter('phase', phases, 'deg')
         # add phase sweep points to the existing sweep points (overwriting
         # them if they exist already)
         sweep_points.update(hard_sweep_dict + [{}])
