04b4866eb7b5318d488cfb83ae3469e407a2eedd
Nathan Lacroix <lna@student.ethz.ch>
2020-10-23 15:39:04 +0200

    Add flux channel to channels of the readout with flux pulse pulse

--- pycqed/measurement/waveform_control/pulse_library.py
+++ pycqed/measurement/waveform_control/pulse_library.py
@@ -1071,6 +1071,7 @@ class GaussFilteredCosIQPulseWithFlux(GaussFilteredCosIQPulse):
                          name=name,
                          **kw)
         self.flux_channel = flux_channel
+        self.channels.append(flux_channel)
         self.flux_pulse_length = self.pulse_length + self.flux_extend_start + self.flux_extend_end
         self.flux_buffer_length_start = self.buffer_length_start - self.flux_extend_start
         self.flux_buffer_length_end = self.length - self.flux_buffer_length_start - self.flux_pulse_length
