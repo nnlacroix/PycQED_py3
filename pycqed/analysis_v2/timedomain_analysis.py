25be17e559a4d48bee8157a2ebc632c3b4b5d2d7
Christoph Hellings <chellings@phys.ethz.ch>
2020-10-30 21:21:14 +0100

    FluxPulseScopeAnalysis: default sign_of_peaks to +1 if column_PCA is used This change is due to the new background sign convention in column_PCA.

--- pycqed/analysis_v2/timedomain_analysis.py
+++ pycqed/analysis_v2/timedomain_analysis.py
@@ -7859,7 +7859,7 @@ class FluxPulseScopeAnalysis(MultiQubit_TimeDomain_Analysis):
             self.sign_of_peaks = {qbn: None for qbn in self.qb_names}
         for qbn in self.qb_names:
             if self.sign_of_peaks[qbn] is None:
-                if self.rotation_type == 'fixed_cal_points':
+                if self.rotation_type in ['column_PCA', 'fixed_cal_points']:
                     # e state corresponds to larger values than g state
                     self.sign_of_peaks[qbn] = 1
                 else:
