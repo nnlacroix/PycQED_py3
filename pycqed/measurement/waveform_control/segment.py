99aa75d3a24400d748931c3e28d5553e3297cf27
Christoph Hellings <chellings@phys.ethz.ch>
2021-01-28 10:33:36 +0100

    WIP: first draft of segment filtering

--- pycqed/measurement/waveform_control/segment.py
+++ pycqed/measurement/waveform_control/segment.py
@@ -46,6 +46,7 @@ class Segment:
         self.elements_on_awg = {}
         self.distortion_dicts = {}
         self.sweep_params = {}
+        self.allow_filter = False
         self.trigger_pars = {
             'pulse_length': self.trigger_pulse_length,
             'amplitude': self.trigger_pulse_amplitude,
