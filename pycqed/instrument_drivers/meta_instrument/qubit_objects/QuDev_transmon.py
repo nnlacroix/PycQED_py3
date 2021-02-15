c68b15fe75a9de49997cf5ed8d3fcee3265d5b7c
Stefania Lazar <stefania.lazar@phys.ethz.ch>
2021-02-15 17:46:07 +0100

    QuDev_transmon: fixes bug in update_detector_functions where classifier params and state prob mtx were not passed as lists to UHF_classifier_detector

--- pycqed/instrument_drivers/meta_instrument/qubit_objects/QuDev_transmon.py
+++ pycqed/instrument_drivers/meta_instrument/qubit_objects/QuDev_transmon.py
@@ -442,8 +442,8 @@ class QuDev_transmon(Qubit):
             channels=channels, nr_shots=self.acq_averages(),
             integration_length=self.acq_length(),
             get_values_function_kwargs={
-                'classifier_params': self.acq_classifier_params(),
-                'state_prob_mtx': self.acq_state_prob_mtx()
+                'classifier_params': [self.acq_classifier_params()],
+                'state_prob_mtx': [self.acq_state_prob_mtx()]
             })
 
         self.int_avg_det = det.UHFQC_integrated_average_detector(
